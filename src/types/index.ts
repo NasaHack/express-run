62b3bdb184ee25f20df8c9daee432244b4b500ef
NasaHack <proah.nasahack@gmail.com>
2025-03-27 23:16:14 +0000

    chore(types): export security.guards.types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
-import "./global.type.ts";
+export * from "./global.type";
 export * from "./specific_to/collections.types";
 export * from "./specific_to/auth.controller.types";
+export * from "./specific_to/security.guards.types";
