be0fd71fdaf6b03b04c9cc22ed32a550e3ec1813
NasaHack <proah.nasahack@gmail.com>
2025-03-27 23:12:43 +0000

    chore(middleware): export securityGuards middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,7 @@
 export * from "./auth/isAuth";
 export * from "./common/multer";
 export * from "./common/underconstructon";
+export * from "./common/securityGuards";
 
 import Auth from "./validator/auth.validator";
 export const validator = {
