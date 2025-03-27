ed13168683f07154ed5c9f2aeab855215d372d84
NasaHack <proah.nasahack@gmail.com>
2025-03-27 23:06:53 +0000

    refactor(app.ts): move cors middleware inside securityGuards wrapper middleware and wrap the express app by securityGuards

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,17 @@
 import { join } from "path";
 import express, { static as static_ } from "express";
-import cors from "cors";
-import { corsOptions } from "./shared";
-import Controllers from "./controllers";
-import api_routes from "./routes/api.routes";
-/*...................XXX............................*/
+import Controllers from "@/controllers";
+import api_routes from "@/routes/api.routes";
+import { securityGuards } from "@/middleware";
+/*...............XXX.........................*/
 
-const app = express();
+const app = securityGuards(express());
 
 const readyToServe = () => {
-  app.use(cors({ ...corsOptions, credentials: true }));
   app.use(static_(join(process.cwd(), "public")));
   app.set("json spaces", 2);
   app.get("/", Controllers.Common.rootResponse);
   app.use("/api", api_routes);
-
   app.use("*", Controllers.Common.notFound);
   app.use(Controllers.Common.defaultError);
 };
