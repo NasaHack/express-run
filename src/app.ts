46a0f1624a1c7c148981699a0bd889c83eb68176
NasaHack <proah.nasahack@gmail.com>
2025-03-28 20:22:23 +0000

    refactor(app): separate secure main app and WebHook app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import { join } from "path";
-import express, { static as static_ } from "express";
+import { static as static_ } from "express";
 import Controllers from "@/controllers";
 import api_routes from "@/routes/api.routes";
 import { securityGuards } from "@/middleware";
+import web_hook_routes from "./routes/web-hook.routes";
 /*...............XXX.........................*/
 
-const app = securityGuards(express());
+const { app, appForWebHook } = securityGuards;
 
 const readyToServe = () => {
-  app.use(static_(join(process.cwd(), "public")));
   app.set("json spaces", 2);
   app.get("/", Controllers.Common.rootResponse);
+  app.use(static_(join(process.cwd(), "public")));
+  app.use("/web-hook", appForWebHook.use(web_hook_routes));
   app.use("/api", api_routes);
   app.use("*", Controllers.Common.notFound);
   app.use(Controllers.Common.defaultError);
