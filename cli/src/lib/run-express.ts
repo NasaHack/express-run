6dbffbd7d86508832a67caae95c6ceece5c5eca9
NasaHack <proah.nasahack@gmail.com>
2025-03-30 02:09:27 +0000

    refactor(cli/src/lib/run-express: improve error handling, exceptions handling

--- cli/src/lib/run-express.ts
+++ cli/src/lib/run-express.ts
@@ -1,61 +1,112 @@
 #!/usr/bin/env node
+import colors from "colors";
 import { RUN_EXPRESS_SRC } from "../constants";
 import {
+  hasExistAnyFile,
   commander,
   getInput,
-  hasCode,
   isSameFolderExist,
-  platform,
   updatePackageName,
+  Logger,
+  pathResolver,
+  shellType,
 } from "./utils";
 
-const executeCommand = (command: string, successMsg: string): boolean => {
-  let isSuccess = false;
-
-  commander(command, (error: Error, isSuccessFlag: boolean) => {
-    if (!error && isSuccessFlag) {
-      console.log(successMsg);
-      isSuccess = true;
-    } else {
-      isSuccess = false;
-    }
-  });
-
-  return isSuccess;
-};
-
 const openProject = (projectName: string) => {
-  updatePackageName(projectName);
-  const command =
-    platform === "win32"
-      ? hasCode
-        ? `cd ${projectName} && rm -rf .git && rm -rf cli  && npm install && code .`
-        : `cd ${projectName} && rm -rf .git && rm -rf cli && start .`
-      : hasCode
-      ? `cd ${projectName} && rm -rf .git && rm -rf cli && npm install && code .`
-      : `cd ${projectName} && rm -rf .git && rm -rf cli && open .`;
+  let hasCode: boolean = false;
+  let commands: string = "";
 
-  let open = executeCommand(command, `🎉 Project opened successfully. 🚀`);
+  try {
+    hasCode = commander("code -v");
 
-  if (!open) {
-    console.log(`\n🚧 cd ${projectName}\n🔧 npm install\n💻 open .`);
+    switch (shellType) {
+      case "powershell":
+        commands = hasCode ? `code .` : `Start-Process .`;
+        break;
+      case "cmd":
+        commands = hasCode ? `code .` : `start .`;
+        break;
+      case "bash":
+        commands = hasCode ? `code .` : `open .`;
+        break;
+      default:
+        commands = "";
+    }
+
+    let oppened = commander(commands);
+
+    if (oppened) {
+      return Logger.success("Project opened successfully!");
+    } else {
+      Logger.warning("Faild to opened project! Try to open project manually!");
+    }
+  } catch (error) {
+    Logger.warning("Faild to opened project! Try to open project manually!");
+  }
+
+  Logger.info(
+    `Try these commands to open project:\n\n⚡️ cd ${projectName}\n🔧 npm install\n💻 ${commands}\n`
+  );
+};
+
+const sanitizeProject = (projectName: string) => {
+  try {
+    let commands: string = "";
+
+    switch (shellType) {
+      case "powershell":
+        commands = `powershell -Command Push-Location ${projectName}; Remove-Item -Path '.git' -Recurse -Force; Remove-Item -Path 'cli' -Recurse -Force; npm install; Pop-Location`;
+        break;
+      case "bash":
+        commands = `cd ${projectName} && rm -rf .git && rm -rf cli && npm install`;
+        break;
+      case "cmd":
+        commands = `cd /d ${projectName} && rmdir /S /Q .git && rmdir /S /Q cli && npm install`;
+        break;
+      default: {
+        commands = "";
+        Logger.warning(
+          "Unsupported shell type! Consider to open project manually! and run npm install commands to install necessary dependencies."
+        );
+      }
+    }
+
+    Logger.success(
+      "Project sanitized and installed dependencies successfully!"
+    );
+    commands && commander(commands) && openProject(projectName);
+  } catch (error) {
+    Logger.warning("Project sanitization failed!");
   }
 };
 
-const cloneRepository = (projectName: string) => {
-  if (!isSameFolderExist(projectName)) {
-    executeCommand(
-      `git clone ${RUN_EXPRESS_SRC} ./${projectName}`,
-      `🎉 Project '${projectName}' cloned successfully! 🚀`
-    );
-    openProject(projectName);
-  } else {
-    console.log(
-      `❌ Project '${projectName}' already exists. Please choose a different name. 🔄`
+const cloneRunExprss = (projectName: string) => {
+  let cloned = commander(`git clone ${RUN_EXPRESS_SRC} ${projectName}`);
+  if (cloned) {
+    Logger.success("Project cloned successfully!");
+    updatePackageName(pathResolver(projectName, "package.json"));
+    Logger.success(
+      `Package name updated to ${colors.bold(projectName).underline}`
     );
   }
+  sanitizeProject(projectName);
 };
 
-getInput("🔍 Enter project name: ", (name: string) => {
-  cloneRepository(name.trim());
+const initialize = (projectName: string) => {
+  if (projectName === "." && hasExistAnyFile())
+    return Logger.error(
+      "Current diretory is not empty! Please consider using a unique name for your project."
+    );
+
+  if (isSameFolderExist(projectName))
+    return Logger.error(
+      "Already a folder exist with the same name! Please consider using a unique name for your project."
+    );
+
+  cloneRunExprss(projectName);
+};
+
+getInput("Enter project name: ", (answer) => {
+  const projectName = answer.trim();
+  initialize(projectName);
 });
