91a803afe949c2988708e3fa7609f2942eb53de6
NasaHack <proah.nasahack@gmail.com>
2025-03-31 18:13:53 +0000

    chore(cli/src/constants): adds FINAL_MESSAGE constants

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,2 +1,4 @@
 export const RUN_EXPRESS_SRC: string =
   "https://github.com/NasaHack/express-run";
+
+export const FINAL_MESSAGE: string = `RGVhciBGcmllbmRzISBpZiB5b3UgZm91bmQgdGhpcyBoZWxwZnVsIHRoZW4gc3RhciB3aWxsIGJlIGFwcHJlY2lhdGVkIPCfpbAuLiAbXTg7O2h0dHBzOi8vZ2l0aHViLmNvbS9OYXNhSGFjay9ydW4tZXhwcmVzcwfirZAgU3RhciBvbiBHaXRIdWIbXTg7OwcK`;
