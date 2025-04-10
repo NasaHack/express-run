7d4bb39485c82f41e9c9fc9846ec994f7034fbe5
NasaHack <proah.nasahack@gmail.com>
2025-04-10 05:18:32 +0000

    refactor(middleware/_zSchemaValidator.ts): adds support for validating query and params with zod.

--- src/middleware/_zSchemaValidator.ts
+++ src/middleware/_zSchemaValidator.ts
@@ -1,10 +1,57 @@
+import { AppError } from "@/shared";
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
 
+const isUrlEncodedOrMultipart = (req: Request) => {
+  const contentType = req.headers["content-type"];
+
+  return (
+    contentType &&
+    (contentType.startsWith("application/x-www-form-urlencoded") ||
+      contentType.startsWith("multipart/form-data"))
+  );
+};
+
 export const zSchemaValidator = (schema: AnyZodObject) => {
   return (req: Request, $: Response, next: NextFunction) => {
+    if (isUrlEncodedOrMultipart(req)) {
+      req.body = { body: { ...req.body } };
+    }
+
     try {
-      schema.parse(req.body);
+      const parsedBody =
+        schema.shape.body && schema.shape.body.safeParse
+          ? schema.shape.body.safeParse(req.body.body)
+          : { success: true };
+
+      const parsedQuery =
+        schema.shape.query && schema.shape.query.safeParse
+          ? schema.shape.query.safeParse(req.query)
+          : { success: true };
+
+      const parsedParams =
+        schema.shape.params && schema.shape.params.safeParse
+          ? schema.shape.params.safeParse(req.params)
+          : { success: true };
+
+      if (
+        !parsedBody.success ||
+        !parsedQuery.success ||
+        !parsedParams.success
+      ) {
+        const sources = [
+          ...(parsedBody.success ? [] : parsedBody.error.issues),
+          ...(parsedQuery.success ? [] : parsedQuery.error.issues),
+          ...(parsedParams.success ? [] : parsedParams.error.issues),
+        ];
+
+        throw new AppError(400, "Zod Validation Error!", sources);
+      }
+
+      if (parsedBody.success) req.body = parsedBody.data;
+      if (parsedQuery.success) req.query = parsedQuery.data;
+      if (parsedParams.success) req.params = parsedParams.data;
+
       next();
     } catch (error) {
       next(error);
